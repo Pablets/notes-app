a596116d278978bdefdeb567ec2f5e43ea49f301
Pablets <godoypablom@gmail.com>
2021-03-13 23:37:35 -0300

    uploading minor changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const handleChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const handleClick = (event) => {
+    event.preventDefault()
+    const noteToAddToState = {
+      id: notes.length + 1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+    }
+    setNotes(prevNotes => prevNotes.concat(noteToAddToState))
+    setNewNote('')
+  }
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Notes</h1>
+      <button onClick={() => setShowAll(!showAll)}>Filtrar</button>
+      <ul>
+        {notesToShow.map((note) => (
+          <li key={note.id}>
+            {note.content}
+            <ul>
+              <li>{note.date}</li>
+              <li
+                style={
+                  note.important
+                    ? { backgroundColor: 'tomato' }
+                    : { backgroundColor: 'gray' }
+                }
+              >
+                {note.important ? 'important' : 'normal'}
+              </li>
+            </ul>
+          </li>
+        ))}
+      </ul>
+      <form onSubmit={handleClick}>
+        <input value={newNote} type='text' onChange={handleChange} />
+        <button type='submit'>Crear nota</button>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
